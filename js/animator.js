721dcb481eacf5a753e22b5725b1e6f1b6bd1cca
Tim Fratangelo <tfrat84@gmail.com>
2015-09-18 20:36:27 -0400

    Fixed an infinite loop bug

--- js/animator.js
+++ js/animator.js
@@ -2,16 +2,17 @@ function animate (scene, renderer, duration, animation) {
 
   var initialTime = Date.now()
 
-  var render = function (duration) {
+  var render = function () {
     requestAnimationFrame( render )
 
     var t = (Date.now() - initialTime) / 1000
 
     if (t <= duration) {
+      console.log(t + " " + duration)
       animation(t)
-      renderer.render( scene, camera )
     }
+    renderer.render( scene, camera )
   }
 
-  render(duration);
+  render();
 }
