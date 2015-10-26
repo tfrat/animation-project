63ab78f09ffc157bc18ca7717384a9ab5bbba3d0
Tim Fratangelo <tfrat84@gmail.com>
2015-10-26 14:40:51 -0400

    The friction force now stops the ball.

--- js/physics.js
+++ js/physics.js
@@ -1,26 +1,50 @@
 
 class PhysicsObject {
-  constructor(object, mass, velocity, acceleration) {
+
+  constructor(object, mass, force) {
+    this.inMotion = true
+    this.uk = .09
+    this.us = .5
+    this.gravity = 9.8
     this.object = object
     this.mass = mass
-    this.velocity = velocity
-    this.acceleration = acceleration
-    this.force = acceleration.clone().multiplyScalar(mass)
-    this.momentum = velocity.clone().multiplyScalar(mass)
+    this.force = force
+    this.momentum = this.force.clone().multiplyScalar(1/mass)
+    this.velocity = this.momentum.clone().multiplyScalar(1/mass)
+    this.normal = this.mass * this.gravity
+    this.staticForce = this.normal * this.us
+    this.kineticForce = this.normal * this.uk
   }
 
   getPosition() {
     return this.object.position
   }
   updateForces( t ) {
-    this.force = this.acceleration.clone().multiplyScalar(this.mass)
+    if(this.inMotion) {
+      var fn = this.force.clone().normalize()
+      fn.negate()
+      fn.multiplyScalar(this.kineticForce)
+      this.force.add(fn)
+
+      this.applyForces(t)
+    }
   }
+
   applyForces( t ) {
+    if(this.inMotion) {
+        this.object.position.addScaledVector(this.velocity, t)
+        var delta = this.force.clone().multiplyScalar(t)
 
-      this.object.position.addScaledVector(this.velocity, t)
-      this.momentum.addScaledVector(this.force, t)
-      this.velocity = this.momentum.clone().multiplyScalar(1/this.mass)
+        printVector("Delta", delta)
+        printVector("Momentum", this.momentum)
 
+        if(delta.length() > this.momentum.length()){
+          this.inMotion = false
+        } else {
+          this.momentum.add(delta)
+          this.velocity = this.momentum.clone().multiplyScalar(1/this.mass)
+        }
+      }
   }
   }
 
